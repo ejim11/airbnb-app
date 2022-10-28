29f41f693154bf2ebd7f5889e4631e647c91a738
Ejim Favour <favourejim56@gmail.com>
2022-10-28 12:49:52 +0100

    layout scss file del

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,7 +1,6 @@
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
-import classes from "./Layout.module.scss";
 
 const Layout = () => {
   return (
